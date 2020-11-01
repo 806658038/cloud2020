e56d279191555ca71cf04d4faa3142d27b6fec50
806658038 <806658038@qq.com>
2020-11-01 19:42:20 +0800

    新增方法

--- cloud-api-commons/src/main/java/com/hzh/springcloud/pojo/Test.java
+++ cloud-api-commons/src/main/java/com/hzh/springcloud/pojo/Test.java
@@ -4,7 +4,29 @@ public class Test {
     private String name;
     private Integer id;
 
-    
+    public void d1(){
 
+    }
+
+    public void d2(){
+
+    }
+
+    public void d3(){
+
+    }
+
+    public void d4(){
+
+    }
+
+    public void d5(){
+
+    }
+
+    public static void main(String[] args) {
+        Test t = new Test();
+
+    }
 
 }
